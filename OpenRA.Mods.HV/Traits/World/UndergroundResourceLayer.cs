c3140de0e1b9a32399bd556b02241ffb779429fe
Matthias Mailänder <matthias@mailaender.name>
2020-04-10 12:04:21 +0200

    Add resource management that is not based on neighboring tiles.

---
+++ OpenRA.Mods.HV/Traits/World/UndergroundResourceLayer.cs
@@ -0,0 +1,218 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2020 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Linq;
+using OpenRA.Graphics;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.HV.Traits
+{
+	[Desc("Attach this to the world actor.", "Order of the layers defines the Z sorting.")]
+	public class UndergroundResourceLayerInfo : ITraitInfo, Requires<ResourceTypeInfo>, Requires<BuildingInfluenceInfo>
+	{
+		public virtual object Create(ActorInitializer init) { return new UndergroundResourceLayer(init.Self); }
+	}
+
+	public class UndergroundResourceLayer : IWorldLoaded
+	{
+		static readonly CellContents EmptyCell = default(CellContents);
+
+		readonly World world;
+		readonly BuildingInfluence buildingInfluence;
+
+		protected readonly CellLayer<CellContents> Content;
+
+		public bool IsResourceLayerEmpty { get { return resCells < 1; } }
+
+		int resCells;
+
+		public event Action<CPos, ResourceType> CellChanged;
+
+		public UndergroundResourceLayer(Actor self)
+		{
+			world = self.World;
+			buildingInfluence = self.Trait<BuildingInfluence>();
+
+			Content = new CellLayer<CellContents>(world.Map);
+		}
+
+		int GetAdjacentCellsWith(ResourceType t, CPos cell)
+		{
+			var sum = 0;
+			var directions = CVec.Directions;
+			for (var i = 0; i < directions.Length; i++)
+			{
+				var c = cell + directions[i];
+				if (Content.Contains(c) && Content[c].Type == t)
+					++sum;
+			}
+
+			return sum;
+		}
+
+		public void WorldLoaded(World w, WorldRenderer wr)
+		{
+			var resources = w.WorldActor.TraitsImplementing<ResourceType>()
+				.ToDictionary(r => r.Info.ResourceType, r => r);
+
+			foreach (var cell in w.Map.AllCells)
+			{
+				ResourceType t;
+				if (!resources.TryGetValue(w.Map.Resources[cell].Type, out t))
+					continue;
+
+				if (!AllowResourceAt(t, cell))
+					continue;
+
+				Content[cell] = CreateResourceCell(t, cell);
+			}
+
+			foreach (var cell in w.Map.AllCells)
+			{
+				var type = GetResourceType(cell);
+				if (type != null)
+				{
+					var temp = GetResource(cell);
+					temp.Density = w.Map.Resources[cell].Index;
+
+					Content[cell] = temp;
+				}
+			}
+		}
+
+		public bool AllowResourceAt(ResourceType rt, CPos cell)
+		{
+			if (!world.Map.Contains(cell))
+				return false;
+
+			if (!rt.Info.AllowedTerrainTypes.Contains(world.Map.GetTerrainInfo(cell).Type))
+				return false;
+
+			if (!rt.Info.AllowUnderActors && world.ActorMap.AnyActorsAt(cell))
+				return false;
+
+			if (!rt.Info.AllowUnderBuildings && buildingInfluence.GetBuildingAt(cell) != null)
+				return false;
+
+			if (!rt.Info.AllowOnRamps)
+			{
+				var tile = world.Map.Tiles[cell];
+				var tileInfo = world.Map.Rules.TileSet.GetTileInfo(tile);
+				if (tileInfo != null && tileInfo.RampType > 0)
+					return false;
+			}
+
+			return true;
+		}
+
+		public bool CanSpawnResourceAt(ResourceType newResourceType, CPos cell)
+		{
+			if (!world.Map.Contains(cell))
+				return false;
+
+			var currentResourceType = GetResourceType(cell);
+			return (currentResourceType == newResourceType && !IsFull(cell))
+				|| (currentResourceType == null && AllowResourceAt(newResourceType, cell));
+		}
+
+		CellContents CreateResourceCell(ResourceType t, CPos cell)
+		{
+			world.Map.CustomTerrain[cell] = world.Map.Rules.TileSet.GetTerrainIndex(t.Info.TerrainType);
+			++resCells;
+
+			return new CellContents
+			{
+				Type = t
+			};
+		}
+
+		public void AddResource(ResourceType t, CPos p, int n)
+		{
+			var cell = Content[p];
+			if (cell.Type == null)
+				cell = CreateResourceCell(t, p);
+
+			if (cell.Type != t)
+				return;
+
+			cell.Density = Math.Min(cell.Type.Info.MaxDensity, cell.Density + n);
+			Content[p] = cell;
+
+			if (CellChanged != null)
+				CellChanged(p, cell.Type);
+		}
+
+		public bool IsFull(CPos cell)
+		{
+			var cellContents = Content[cell];
+			return cellContents.Density == cellContents.Type.Info.MaxDensity;
+		}
+
+		public ResourceType Harvest(CPos cell)
+		{
+			var c = Content[cell];
+			if (c.Type == null)
+				return null;
+
+			if (--c.Density < 0)
+			{
+				Content[cell] = EmptyCell;
+				world.Map.CustomTerrain[cell] = byte.MaxValue;
+				--resCells;
+			}
+			else
+				Content[cell] = c;
+
+			if (CellChanged != null)
+				CellChanged(cell, c.Type);
+
+			return c.Type;
+		}
+
+		public void Destroy(CPos cell)
+		{
+			// Don't break other users of CustomTerrain if there are no resources
+			var c = Content[cell];
+			if (c.Type == null)
+				return;
+
+			--resCells;
+
+			// Clear cell
+			Content[cell] = EmptyCell;
+			world.Map.CustomTerrain[cell] = byte.MaxValue;
+
+			if (CellChanged != null)
+				CellChanged(cell, c.Type);
+		}
+
+		public CellContents GetResource(CPos cell) { return Content[cell]; }
+		public ResourceType GetResourceType(CPos cell) { return Content[cell].Type; }
+
+		public int GetResourceDensity(CPos cell) { return Content[cell].Density; }
+		public int GetMaxResourceDensity(CPos cell)
+		{
+			if (Content[cell].Type == null)
+				return 0;
+
+			return Content[cell].Type.Info.MaxDensity;
+		}
+
+		public struct CellContents
+		{
+			public static readonly CellContents Empty = default(CellContents);
+			public ResourceType Type;
+			public int Density;
+		}
+	}
+}
---
+++ OpenRA.Mods.HV/Widgets/Logic/StackedLayerSelectorLogic.cs
@@ -0,0 +1,79 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2020 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Linq;
+using OpenRA.Graphics;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.Common.Widgets;
+using OpenRA.Widgets;
+
+namespace OpenRA.Mods.HV.Widgets.Logic
+{
+	public class StackedLayerSelectorLogic : ChromeLogic
+	{
+		readonly EditorViewportControllerWidget editor;
+		readonly WorldRenderer worldRenderer;
+		readonly EditorCursorLayer editorCursor;
+
+		readonly ScrollPanelWidget layerTemplateList;
+		readonly ScrollItemWidget layerPreviewTemplate;
+
+		[ObjectCreator.UseCtor]
+		public StackedLayerSelectorLogic(Widget widget, WorldRenderer worldRenderer)
+		{
+			this.worldRenderer = worldRenderer;
+			editor = widget.Parent.Get<EditorViewportControllerWidget>("MAP_EDITOR");
+			editorCursor = worldRenderer.World.WorldActor.Trait<EditorCursorLayer>();
+
+			layerTemplateList = widget.Get<ScrollPanelWidget>("LAYERTEMPLATE_LIST");
+			layerTemplateList.Layout = new GridLayout(layerTemplateList);
+			layerPreviewTemplate = layerTemplateList.Get<ScrollItemWidget>("LAYERPREVIEW_TEMPLATE");
+
+			IntializeLayerPreview(widget);
+		}
+
+		void IntializeLayerPreview(Widget widget)
+		{
+			layerTemplateList.RemoveChildren();
+			var rules = worldRenderer.World.Map.Rules;
+			var resources = rules.Actors["world"].TraitInfos<ResourceTypeInfo>();
+			var tileSize = worldRenderer.World.Map.Grid.TileSize;
+			foreach (var resource in resources)
+			{
+				var newResourcePreviewTemplate = ScrollItemWidget.Setup(layerPreviewTemplate,
+					() => editorCursor.Type == EditorCursorType.Resource && editorCursor.Resource == resource,
+					() => editor.SetBrush(new StackedEditorResourceBrush(editor, resource, worldRenderer)));
+
+				newResourcePreviewTemplate.Bounds.X = 0;
+				newResourcePreviewTemplate.Bounds.Y = 0;
+
+				var layerPreview = newResourcePreviewTemplate.Get<SpriteWidget>("LAYER_PREVIEW");
+				layerPreview.IsVisible = () => true;
+				layerPreview.GetPalette = () => resource.Palette;
+
+				var variant = resource.Sequences.FirstOrDefault();
+				var sequence = rules.Sequences.GetSequence("resources", variant); // TODO: choose something more appropriate
+				var frame = sequence.Frames != null ? sequence.Frames.Last() : resource.MaxDensity - 1;
+				layerPreview.GetSprite = () => sequence.GetSprite(frame);
+
+				layerPreview.Bounds.Width = tileSize.Width;
+				layerPreview.Bounds.Height = tileSize.Height;
+				newResourcePreviewTemplate.Bounds.Width = tileSize.Width + (layerPreview.Bounds.X * 2);
+				newResourcePreviewTemplate.Bounds.Height = tileSize.Height + (layerPreview.Bounds.Y * 2);
+
+				newResourcePreviewTemplate.IsVisible = () => true;
+				newResourcePreviewTemplate.GetTooltipText = () => resource.Type;
+
+				layerTemplateList.AddChild(newResourcePreviewTemplate);
+			}
+		}
+	}
+}
