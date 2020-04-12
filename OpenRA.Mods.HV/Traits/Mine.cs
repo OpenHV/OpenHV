e7fd0edd561509d5efb04e79bfa592b4bd29a346
Matthias Mailänder <matthias@mailaender.name>
2020-04-12 22:51:40 +0200

    Add a minelayer.

---
+++ OpenRA.Mods.HV/Traits/Mine.cs
@@ -0,0 +1,75 @@
+#region Copyright & License Information
+/*
+ * Copyright 2019-2020 The OpenHV Developers (see CREDITS)
+ * This file is part of OpenHV, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.HV.Traits
+{
+	class MineInfo : ITraitInfo
+	{
+		public readonly BitSet<CrushClass> CrushClasses = default(BitSet<CrushClass>);
+		public readonly bool AvoidFriendly = true;
+		public readonly bool BlockFriendly = true;
+		public readonly BitSet<CrushClass> DetonateClasses = default(BitSet<CrushClass>);
+
+		public object Create(ActorInitializer init) { return new Mine(this); }
+	}
+
+	class Mine : ICrushable, INotifyCrushed
+	{
+		readonly MineInfo info;
+
+		public Mine(MineInfo info)
+		{
+			this.info = info;
+		}
+
+		void INotifyCrushed.WarnCrush(Actor self, Actor crusher, BitSet<CrushClass> crushClasses) { }
+
+		void INotifyCrushed.OnCrush(Actor self, Actor crusher, BitSet<CrushClass> crushClasses)
+		{
+			if (!info.CrushClasses.Overlaps(crushClasses))
+				return;
+
+			if (crusher.Info.HasTraitInfo<MineImmuneInfo>() || (self.Owner.Stances[crusher.Owner] == Stance.Ally && info.AvoidFriendly))
+				return;
+
+			var mobile = crusher.TraitOrDefault<Mobile>();
+			if (mobile != null && !info.DetonateClasses.Overlaps(mobile.Info.LocomotorInfo.Crushes))
+				return;
+
+			self.Kill(crusher, mobile != null ? mobile.Info.LocomotorInfo.CrushDamageTypes : default(BitSet<DamageType>));
+		}
+
+		bool ICrushable.CrushableBy(Actor self, Actor crusher, BitSet<CrushClass> crushClasses)
+		{
+			if (info.BlockFriendly && !crusher.Info.HasTraitInfo<MineImmuneInfo>() && self.Owner.Stances[crusher.Owner] == Stance.Ally)
+				return false;
+
+			return info.CrushClasses.Overlaps(crushClasses);
+		}
+
+		LongBitSet<PlayerBitMask> ICrushable.CrushableBy(Actor self, BitSet<CrushClass> crushClasses)
+		{
+			if (!info.CrushClasses.Overlaps(crushClasses))
+				return self.World.NoPlayersMask;
+
+			// Friendly units should move around!
+			return info.BlockFriendly ? self.Owner.EnemyPlayersMask : self.World.AllPlayersMask;
+		}
+	}
+
+	[Desc("Tag trait for stuff that should not trigger mines.")]
+	class MineImmuneInfo : TraitInfo<MineImmune> { }
+	class MineImmune { }
+}
---
+++ OpenRA.Mods.HV/Traits/Minelayer.cs
@@ -0,0 +1,274 @@
+#region Copyright & License Information
+/*
+ * Copyright 2019-2020 The OpenHV Developers (see CREDITS)
+ * This file is part of OpenHV, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenRA.Graphics;
+using OpenRA.Mods.Common.Orders;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.HV.Activities;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.HV.Traits
+{
+	public class MinelayerInfo : ITraitInfo, Requires<RearmableInfo>
+	{
+		[FieldLoader.Require]
+		[ActorReference(typeof(MineInfo))]
+		public readonly string[] Mines = null;
+
+		public readonly string AmmoPoolName = "primary";
+
+		public readonly WDist MinefieldDepth = new WDist(1536);
+
+		[VoiceReference]
+		[Desc("Voice to use when ordered to lay a minefield.")]
+		public readonly string Voice = "Action";
+
+		[Desc("Sprite image used for overlays.")]
+		public readonly string TileImage = "overlay";
+
+		[SequenceReference("TileImage")]
+		[Desc("Sprite overlay to use for valid minefield cells.")]
+		public readonly string TileValidName = "target-valid";
+
+		[SequenceReference("TileImage")]
+		[Desc("Sprite overlay to use for invalid minefield cells.")]
+		public readonly string TileInvalidName = "target-invalid";
+
+		[SequenceReference("TileImage")]
+		[Desc("Sprite overlay to use for minefield cells hidden behind fog or shroud.")]
+		public readonly string TileUnknownName = "target-unknown";
+
+		[Desc("Cursor to display when able to (un)deploy the actor.")]
+		public readonly string Cursor = "ability";
+
+		public object Create(ActorInitializer init) { return new Minelayer(init.Self, this); }
+	}
+
+	public class Minelayer : IIssueOrder, IResolveOrder, ISync, IIssueDeployOrder, IOrderVoice, ITick
+	{
+		public readonly MinelayerInfo Info;
+
+		public readonly Sprite Tile;
+
+		[Sync]
+		CPos minefieldStart;
+
+		public Minelayer(Actor self, MinelayerInfo info)
+		{
+			Info = info;
+
+			Tile = self.World.Map.Rules.Sequences.GetSequence("overlay", Info.TileValidName).GetSprite(0);
+		}
+
+		IEnumerable<IOrderTargeter> IIssueOrder.Orders
+		{
+			get
+			{
+				yield return new BeginMinefieldOrderTargeter();
+				yield return new DeployOrderTargeter("PlaceMine", 5);
+			}
+		}
+
+		Order IIssueOrder.IssueOrder(Actor self, IOrderTargeter order, Target target, bool queued)
+		{
+			switch (order.OrderID)
+			{
+				case "BeginMinefield":
+					var start = self.World.Map.CellContaining(target.CenterPosition);
+					if (self.World.OrderGenerator is MinefieldOrderGenerator)
+						((MinefieldOrderGenerator)self.World.OrderGenerator).AddMinelayer(self);
+					else
+						self.World.OrderGenerator = new MinefieldOrderGenerator(self, start, queued);
+
+					return new Order("BeginMinefield", self, Target.FromCell(self.World, start), queued);
+				case "PlaceMine":
+					return new Order("PlaceMine", self, Target.FromCell(self.World, self.Location), queued);
+				default:
+					return null;
+			}
+		}
+
+		Order IIssueDeployOrder.IssueDeployOrder(Actor self, bool queued)
+		{
+			return new Order("PlaceMine", self, Target.FromCell(self.World, self.Location), queued);
+		}
+
+		bool IIssueDeployOrder.CanIssueDeployOrder(Actor self, bool queued) { return true; }
+
+		void IResolveOrder.ResolveOrder(Actor self, Order order)
+		{
+			if (order.OrderString != "BeginMinefield" && order.OrderString != "PlaceMinefield" && order.OrderString != "PlaceMine")
+				return;
+
+			var cell = self.World.Map.CellContaining(order.Target.CenterPosition);
+			if (order.OrderString == "BeginMinefield")
+				minefieldStart = cell;
+			else if (order.OrderString == "PlaceMine")
+				self.QueueActivity(order.Queued, new LayMines(self));
+			else if (order.OrderString == "PlaceMinefield")
+			{
+				var movement = self.Trait<IPositionable>();
+
+				var minefield = GetMinefieldCells(minefieldStart, cell, Info.MinefieldDepth)
+					.Where(c => movement.CanEnterCell(c, null, BlockedByActor.Immovable) || !self.Owner.Shroud.IsVisible(c))
+					.OrderBy(c => (c - minefieldStart).LengthSquared).ToList();
+
+				self.QueueActivity(order.Queued, new LayMines(self, minefield));
+				self.ShowTargetLines();
+			}
+		}
+
+		void ITick.Tick(Actor self)
+		{
+			if (self.CurrentActivity != null)
+				foreach (var field in self.CurrentActivity.ActivitiesImplementing<LayMines>())
+					field.CleanMineField(self);
+		}
+
+		string IOrderVoice.VoicePhraseForOrder(Actor self, Order order)
+		{
+			if (order.OrderString == "PlaceMine" || order.OrderString == "PlaceMinefield")
+				return Info.Voice;
+
+			return null;
+		}
+
+		static IEnumerable<CPos> GetMinefieldCells(CPos start, CPos end, WDist depth)
+		{
+			var mins = new CPos(Math.Min(start.X, end.X), Math.Min(start.Y, end.Y));
+			var maxs = new CPos(Math.Max(start.X, end.X), Math.Max(start.Y, end.Y));
+
+			// TODO: proper endcaps, if anyone cares (which won't happen unless depth is large)
+			var p = end - start;
+			var q = new float2(p.Y, -p.X);
+			q = (start != end) ? (1 / q.Length) * q : new float2(1, 0);
+			var c = -float2.Dot(q, new float2(start.X, start.Y));
+
+			// return all points such that |ax + by + c| < depth
+			for (var i = mins.X; i <= maxs.X; i++)
+				for (var j = mins.Y; j <= maxs.Y; j++)
+					if (Math.Abs(q.X * i + q.Y * j + c) * 1024 < depth.Length)
+						yield return new CPos(i, j);
+		}
+
+		class MinefieldOrderGenerator : OrderGenerator
+		{
+			readonly List<Actor> minelayers;
+			readonly Sprite tileOk;
+			readonly Sprite tileUnknown;
+			readonly Sprite tileBlocked;
+			readonly CPos minefieldStart;
+			readonly bool queued;
+
+			public MinefieldOrderGenerator(Actor a, CPos xy, bool queued)
+			{
+				minelayers = new List<Actor>() { a };
+				minefieldStart = xy;
+				this.queued = queued;
+
+				var minelayer = a.Trait<Minelayer>();
+				tileOk = a.World.Map.Rules.Sequences.GetSequence(minelayer.Info.TileImage, minelayer.Info.TileValidName).GetSprite(0);
+				tileUnknown = a.World.Map.Rules.Sequences.GetSequence(minelayer.Info.TileImage, minelayer.Info.TileUnknownName).GetSprite(0);
+				tileBlocked = a.World.Map.Rules.Sequences.GetSequence(minelayer.Info.TileImage, minelayer.Info.TileInvalidName).GetSprite(0);
+			}
+
+			public void AddMinelayer(Actor a)
+			{
+				minelayers.Add(a);
+			}
+
+			protected override IEnumerable<Order> OrderInner(World world, CPos cell, int2 worldPixel, MouseInput mi)
+			{
+				if (mi.Button == Game.Settings.Game.MouseButtonPreference.Cancel)
+				{
+					world.CancelInputMode();
+					yield break;
+				}
+
+				if (mi.Button == Game.Settings.Game.MouseButtonPreference.Action)
+				{
+					minelayers.First().World.CancelInputMode();
+					foreach (var minelayer in minelayers)
+						yield return new Order("PlaceMinefield", minelayer, Target.FromCell(world, cell), queued);
+				}
+			}
+
+			protected override void Tick(World world)
+			{
+				minelayers.RemoveAll(minelayer => !minelayer.IsInWorld || minelayer.IsDead);
+				if (!minelayers.Any())
+					world.CancelInputMode();
+			}
+
+			protected override IEnumerable<IRenderable> Render(WorldRenderer wr, World world) { yield break; }
+			protected override IEnumerable<IRenderable> RenderAboveShroud(WorldRenderer wr, World world)
+			{
+				var minelayer = minelayers.FirstOrDefault(m => m.IsInWorld && !m.IsDead);
+				if (minelayer == null)
+					yield break;
+
+				// We get the biggest depth so we cover all cells that mines could be placed on.
+				var lastMousePos = wr.Viewport.ViewToWorld(Viewport.LastMousePos);
+				var minefield = GetMinefieldCells(minefieldStart, lastMousePos,
+					minelayers.Max(m => m.Info.TraitInfo<MinelayerInfo>().MinefieldDepth));
+
+				var movement = minelayer.Trait<IPositionable>();
+				var pal = wr.Palette(TileSet.TerrainPaletteInternalName);
+				foreach (var c in minefield)
+				{
+					var tile = tileOk;
+					if (world.FogObscures(c))
+						tile = tileUnknown;
+					else if (!movement.CanEnterCell(c, null, BlockedByActor.Immovable))
+						tile = tileBlocked;
+
+					yield return new SpriteRenderable(tile, world.Map.CenterOfCell(c),
+						WVec.Zero, -511, pal, 1f, true);
+				}
+			}
+
+			protected override IEnumerable<IRenderable> RenderAnnotations(WorldRenderer wr, World world) { yield break; }
+
+			protected override string GetCursor(World world, CPos cell, int2 worldPixel, MouseInput mi)
+			{
+				return minelayers.First().Trait<Minelayer>().Info.Cursor;
+			}
+		}
+
+		class BeginMinefieldOrderTargeter : IOrderTargeter
+		{
+			public string OrderID { get { return "BeginMinefield"; } }
+			public int OrderPriority { get { return 5; } }
+			public bool TargetOverridesSelection(Actor self, Target target, List<Actor> actorsAt, CPos xy, TargetModifiers modifiers) { return true; }
+
+			public bool CanTarget(Actor self, Target target, List<Actor> othersAtTarget, ref TargetModifiers modifiers, ref string cursor)
+			{
+				if (target.Type != TargetType.Terrain)
+					return false;
+
+				var location = self.World.Map.CellContaining(target.CenterPosition);
+				if (!self.World.Map.Contains(location))
+					return false;
+
+				var minelayer = self.Trait<Minelayer>();
+				cursor = minelayer.Info.Cursor;
+				IsQueued = modifiers.HasModifier(TargetModifiers.ForceQueue);
+
+				return modifiers.HasModifier(TargetModifiers.ForceAttack);
+			}
+
+			public bool IsQueued { get; protected set; }
+		}
+	}
+}
