e4340a9af0476bc3b8503bdf1f1d7fde6f417ce2
Matthias Mailänder <matthias@mailaender.name>
2020-06-09 22:35:37 +0200

    Add a pre-release warning.

---
+++ OpenRA.Mods.HV/Widgets/Logic/PreReleaseWarningPrompt.cs
@@ -0,0 +1,37 @@
+#region Copyright & License Information
+/*
+ * Copyright 2019-2020 The OpenHV Developers (see CREDITS)
+ * This file is part of OpenHV, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Mods.Common.Widgets;
+using OpenRA.Widgets;
+
+namespace OpenRA.Mods.HV.Widgets.Logic
+{
+	public class PreReleaseWarningPrompt : ChromeLogic
+	{
+		static bool promptAccepted;
+
+		[ObjectCreator.UseCtor]
+		public PreReleaseWarningPrompt(Widget widget, World world, ModData modData)
+		{
+			if (!promptAccepted && modData.Manifest.Metadata.Version != "{DEV_VERSION}")
+				widget.Get<ButtonWidget>("CONTINUE_BUTTON").OnClick = () => ShowMainMenu(world);
+			else
+				ShowMainMenu(world);
+		}
+
+		void ShowMainMenu(World world)
+		{
+			promptAccepted = true;
+			Ui.ResetAll();
+			Game.LoadWidget(world, "MAINMENU", Ui.Root, new WidgetArgs());
+		}
+	}
+}
