afff2cb95585631d2a81576f4e8f169b658452ca
Matthias Mailänder <matthias@mailaender.name>
2020-07-15 21:10:34 +0200

    Add cli tool to combine sprites while preserving the palette.

---
+++ OpenRA.Mods.HV/UtilityCommands/CombineSprites.cs
@@ -0,0 +1,73 @@
+#region Copyright & License Information
+/*
+ * Copyright 2019-2020 The OpenHV Developers (see CREDITS)
+ * This file is part of OpenHV, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenRA.FileFormats;
+using OpenRA.Primitives;
+
+namespace OpenRA.Mods.Common.UtilityCommands
+{
+	class CombineSprites : IUtilityCommand
+	{
+		string IUtilityCommand.Name { get { return "--combine-png"; } }
+
+		bool IUtilityCommand.ValidateArguments(string[] args)
+		{
+			return args.Length >= 2;
+		}
+
+		[Desc("PATH INPUT-FILENAME-PATTERN OUTPUT-FILENAME",
+			  "Combine a series of indexed PNGs described.")]
+		void IUtilityCommand.Run(Utility utility, string[] args)
+		{
+			// HACK: The engine code assumes that Game.modData is set.
+			var modData = Game.ModData = utility.ModData;
+
+			var width = 0;
+			var height = 0;
+			var data = new byte[0];
+			var palette = new Color[0];
+			var embeddedData = new Dictionary<string, string>();
+
+			var sprites = Directory.GetFiles(args[1], args[2], SearchOption.TopDirectoryOnly);
+			Array.Sort(sprites);
+			foreach (var sprite in sprites)
+			{
+				using (var s = File.OpenRead(sprite))
+				{
+					var png = new Png(s);
+
+					if (png.Palette == null)
+						throw new InvalidDataException("Non-Palette indexed PNG are not supported.");
+
+					width = png.Width;
+					height += png.Height;
+
+					var combined = new byte[data.Length + png.Data.Length];
+					Array.Copy(data, combined, data.Length);
+					Array.Copy(png.Data, 0, combined, data.Length, png.Data.Length);
+
+					data = combined;
+
+					palette = png.Palette;
+
+					embeddedData = png.EmbeddedData;
+				}
+			}
+
+			var output = new Png(data, width, height, palette, embeddedData);
+			var path = Path.Combine(args[1], args[3]);
+			output.Save(path);
+		}
+	}
+}
