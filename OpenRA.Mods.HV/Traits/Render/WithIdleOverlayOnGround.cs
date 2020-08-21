57f14586a1ee8453074ec7dc195bbd96a96f47f4
Zimmermann Gyula <graiondilach@hotmail.com>
2020-08-21 13:28:30 +0200

    Add WIOOnGround from AS to correct Balloon shadow.

---
+++ OpenRA.Mods.HV/Traits/Render/WithIdleOverlayOnGround.cs
@@ -0,0 +1,96 @@
+#region Copyright & License Information
+/*
+ * Copyright 2019-2020 The OpenHV Developers (see CREDITS)
+ * This file is part of OpenHV, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using OpenRA.Graphics;
+using OpenRA.Mods.Common;
+using OpenRA.Mods.Common.Graphics;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.Common.Traits.Render;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.HV.Traits.Render
+{
+	[Desc("Plays an idle overlay on the ground position under the actor (regardless of it's actual height).")]
+	public class WithIdleOverlayOnGroundInfo : WithIdleOverlayInfo
+	{
+		public override object Create(ActorInitializer init) { return new WithIdleOverlayOnGround(init.Self, this); }
+
+		public new IEnumerable<IActorPreview> RenderPreviewSprites(ActorPreviewInitializer init, RenderSpritesInfo rs, string image, int facings, PaletteReference p)
+		{
+			if (!EnabledByDefault)
+				yield break;
+
+			if (Palette != null)
+				p = init.WorldRenderer.Palette(Palette);
+
+			Func<WAngle> facing;
+			var dynamicfacingInit = init.GetOrDefault<DynamicFacingInit>(this);
+			if (dynamicfacingInit != null)
+				facing = dynamicfacingInit.Value;
+			else
+			{
+				var f = init.GetValue<FacingInit, WAngle>(this, WAngle.Zero);
+				facing = () => f;
+			}
+
+			var anim = new Animation(init.World, image, facing);
+			anim.IsDecoration = IsDecoration;
+			anim.PlayRepeating(RenderSprites.NormalizeSequence(anim, init.GetDamageState(), Sequence));
+
+			var body = init.Actor.TraitInfo<BodyOrientationInfo>();
+			Func<WRot> orientation = () => body.QuantizeOrientation(WRot.FromYaw(facing()), facings);
+			Func<WVec> offset = () => body.LocalToWorld(Offset.Rotate(orientation()));
+			Func<int> zOffset = () =>
+			{
+				var tmpOffset = offset();
+				return tmpOffset.Y + tmpOffset.Z + 1;
+			};
+
+			yield return new SpriteActorPreview(anim, offset, zOffset, p, rs.Scale);
+		}
+	}
+
+	public class WithIdleOverlayOnGround : PausableConditionalTrait<WithIdleOverlayOnGroundInfo>, INotifyDamageStateChanged
+	{
+		readonly Animation overlay;
+
+		public WithIdleOverlayOnGround(Actor self, WithIdleOverlayOnGroundInfo info)
+			: base(info)
+		{
+			var rs = self.Trait<RenderSprites>();
+			var body = self.Trait<BodyOrientation>();
+
+			var image = info.Image ?? rs.GetImage(self);
+			overlay = new Animation(self.World, image, () => IsTraitPaused);
+			overlay.IsDecoration = info.IsDecoration;
+			if (info.StartSequence != null)
+				overlay.PlayThen(RenderSprites.NormalizeSequence(overlay, self.GetDamageState(), info.StartSequence),
+					() => overlay.PlayRepeating(RenderSprites.NormalizeSequence(overlay, self.GetDamageState(), info.Sequence)));
+			else
+				overlay.PlayRepeating(RenderSprites.NormalizeSequence(overlay, self.GetDamageState(), info.Sequence));
+
+			var anim = new AnimationWithOffset(overlay,
+				() => body.LocalToWorld(info.Offset.Rotate(body.QuantizeOrientation(self, self.Orientation)))
+					- new WVec(WDist.Zero, WDist.Zero, self.World.Map.DistanceAboveTerrain(self.CenterPosition)),
+				() => IsTraitDisabled,
+				p => RenderUtils.ZOffsetFromCenter(self, p, 1));
+
+			rs.Add(anim, info.Palette, info.IsPlayerPalette);
+		}
+
+		void INotifyDamageStateChanged.DamageStateChanged(Actor self, AttackInfo e)
+		{
+			overlay.ReplaceAnim(RenderSprites.NormalizeSequence(overlay, e.DamageState, overlay.CurrentSequence.Name));
+		}
+	}
+}
