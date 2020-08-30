12ea88b089e9b4debaca6c07f9f8393129773e6a
dnqbob <dnqbob@163.com>
2020-08-30 21:25:42 +0800

    Add pre-spawn clouds function to CloudSpawner

--- OpenRA.Mods.HV/Traits/World/CloudSpawner.cs
+++ OpenRA.Mods.HV/Traits/World/CloudSpawner.cs
@@ -9,6 +9,7 @@
  */
 #endregion
 
+using System.Linq;
 using OpenRA.Graphics;
 using OpenRA.Mods.HV.Effects;
 using OpenRA.Traits;
@@ -53,10 +54,12 @@ namespace OpenRA.Mods.HV.Traits
 		[Desc("Distance margin where the cloud can be removed.")]
 		public readonly WDist CloseEnough = new WDist(128);
 
+		[Desc("Should we pre-spawn clouds covers the map?")]
+		public readonly bool ShouldPrespawn = true;
 		public override object Create(ActorInitializer init) { return new CloudSpawner(this); }
 	}
 
-	public class CloudSpawner : ITick
+	public class CloudSpawner : ITick, IWorldLoaded
 	{
 		readonly CloudSpawnerInfo info;
 
@@ -79,6 +82,12 @@ namespace OpenRA.Mods.HV.Traits
 			}
 		}
 
+		void IWorldLoaded.WorldLoaded(World w, WorldRenderer wr)
+		{
+			if (info.ShouldPrespawn)
+				PreSpawnClouds(w);
+		}
+
 		void SpawnCloud(Actor self)
 		{
 			var position = self.World.Map.ChooseRandomCell(self.World.SharedRandom);
@@ -94,6 +103,73 @@ namespace OpenRA.Mods.HV.Traits
 			animation.PlayRepeating(info.Sequences.Random(self.World.SharedRandom));
 
 			self.World.AddFrameEndTask(w => w.Add(new Cloud(self.World, animation, startEdge, finishEdge, facing, info)));
+		}
+
+		void PreSpawnClouds(World world)
+		{
+			var facing = 256 * info.WindDirection / 32;
+			var delta = new WVec(0, -1024, 0).Rotate(WRot.FromFacing(facing));
+
+			/*
+			 * The following codes find the middle point in map. Because clouds must
+			 * start from an edge to another edge, the paths of clouds
+			 * which contain this point will always be the longest path no matter the direction,
+			 * as long as the map is a symmetrical shape.
+			 *
+			 * For example, the middle point in map is "@", @ is (u = X/2, v = Y/2)
+			 * You can imagine any wind direction with all edges clouds possibly spawn.
+			 *
+			 *              ____X____
+			 *				|		|
+			 *				|		|
+			 *				|	@	Y
+			 *				|		|
+			 *				|_______|
+			 *
+			 * By using this longest path, we can figure out the number of clouds should
+			 * spawn when a certain cloud completely goes over a longest path, which should be
+			 * neither too little nor too big compared with the clouds per map-cell later
+			 * spawned by `SpawnCloud`.
+			 */
+			var middlePoint = new MPos(world.Map.MapSize.X / 2, world.Map.MapSize.Y / 2);
+			var middlePointTarget = world.Map.CenterOfCell(middlePoint.ToCPos(world.Map));
+
+			// lDistance and averageSpeed used are for loop condition below.
+			var longestCloudDistance = System.Math.Abs(
+				(world.Map.DistanceToEdge(middlePointTarget, -delta) + info.Cordon).Length
+				+ (world.Map.DistanceToEdge(middlePointTarget, delta) + info.Cordon).Length);
+			var averageCloudSpeed = System.Math.Abs((int)info.Speed.Average(s => s.Length));
+
+			var stepPerSpawn = averageCloudSpeed * info.SpawnInterval;
+			var stepPerSpawnVector = stepPerSpawn * delta / 1024;
+
+			/*
+			 * Spawn clouds.
+			 *
+			 * Try to make clouds spawning cover the entire map, meanwhile
+			 * with some randomization. Choose random spawning point and
+			 * find startEdge, then add offset to let they go further to cover
+			 * the map. The offset will be increased to simulate the movement
+			 * the cloud would have made otherwise.
+			 */
+			var offset = WVec.Zero;
+			while (longestCloudDistance > 0)
+			{
+				var position = world.Map.ChooseRandomCell(world.SharedRandom);
+				var target = world.Map.CenterOfCell(position) + new WVec(0, 0, info.CruiseAltitude.Length);
+
+				var startEdge = target - (world.Map.DistanceToEdge(target, -delta) + info.Cordon).Length * delta / 1024;
+				startEdge += offset;
+				var finishEdge = target + (world.Map.DistanceToEdge(target, delta) + info.Cordon).Length * delta / 1024;
+
+				var animation = new Animation(world, info.Image, () => WAngle.FromFacing(facing));
+				animation.PlayRepeating(info.Sequences.Random(world.SharedRandom));
+
+				world.AddFrameEndTask(w => w.Add(new Cloud(world, animation, startEdge, finishEdge, facing, info)));
+
+				offset += stepPerSpawnVector;
+				longestCloudDistance -= stepPerSpawn;
+			}
 		}
 	}
 }
