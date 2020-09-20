de3ffc3b1281c33773db042a48d97e3dadbb070f
Matthias Mailänder <matthias@mailaender.name>
2020-09-20 18:09:23 +0200

    Unbrand application name.

--- packaging/macos/buildpackage.sh
+++ packaging/macos/buildpackage.sh
@@ -76,7 +76,7 @@ else
 fi
 
 BUILTDIR="${PACKAGING_DIR}/build"
-PACKAGING_OSX_APP_NAME="OpenRA - ${PACKAGING_DISPLAY_NAME}.app"
+PACKAGING_OSX_APP_NAME="${PACKAGING_DISPLAY_NAME}.app"
 
 # Set the working dir to the location of this script
 cd "${PACKAGING_DIR}"
