9b41db38f299c4356ad458a24e7e39c3b843e111
Matthias Mailänder <matthias@mailaender.name>
2021-01-09 23:16:35 +0100

    Only distribute the download link and add install instructions.

---
+++ packaging/windows/chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'openhv'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = '{URL}'
+$checksum = '{MD5CHECKSUM}'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ packaging/windows/chocolatey/tools/chocolateyuninstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'openhv'
+$packageSearch = "OpenHV"
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   { $_.DisplayName -like "$packageSearch" } `
+| ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                               -FileType "$installerType" `
+                                               -SilentArgs "$($silentArgs)" `
+                                               -File "$($_.UninstallString.Replace('"',''))" `
+                                               -ValidExitCodes $validExitCodes }
