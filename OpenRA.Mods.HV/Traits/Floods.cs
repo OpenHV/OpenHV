0fe626e90b4b85d00bbe048bc0e25e81175c802f
Matthias Mailänder <matthias@mailaender.name>
2021-12-27 22:05:32 +0100

    Add water flow inside confinements.
    Closes #410

--- OpenRA.Mods.HV/Traits/Floods.cs
+++ OpenRA.Mods.HV/Traits/Floods.cs
@@ -9,20 +9,35 @@
  */
 #endregion
 
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using OpenRA.Mods.Common;
+using OpenRA.Mods.Common.Terrain;
 using OpenRA.Mods.Common.Traits;
 using OpenRA.Traits;
 
 namespace OpenRA.Mods.HV.Traits
 {
-	[Desc("Lets the actor spread resources around it in straight lines.")]
+	[Desc("Lets the actor spread liquid tiles around it in straight lines.")]
 	class FloodsInfo : ConditionalTraitInfo
 	{
-		public readonly int Interval = 75;
-		public readonly string ResourceType = "water";
-		public readonly int MaxRange = 100;
+		[Desc("Speed in ticks between each wave.")]
+		public readonly int Interval = 10;
+
+		[Desc("After which amount of flooded tiles to stop traversing further.")]
+		public readonly int MaxTiles = 10000;
+
+		[Desc("Which tile ID to replace with which flooded variant")]
+		public readonly Dictionary<ushort, ushort> ReplacementTiles = new Dictionary<ushort, ushort>();
+
+		[Desc("Terrain type that should be converted albeit unpathable.")]
+		public readonly string[] StopTerrainTypes = Array.Empty<string>();
+
+		[FieldLoader.Require]
+		[Desc("Locomotor to test pathability of barriers against.")]
+		public readonly string Locomotor = null;
 
 		public override object Create(ActorInitializer init) { return new Floods(init.Self, this); }
 	}
@@ -30,50 +45,76 @@ namespace OpenRA.Mods.HV.Traits
 	class Floods : ConditionalTrait<FloodsInfo>, ITick
 	{
 		readonly FloodsInfo info;
-
-		readonly IResourceLayer resourceLayer;
-
-		readonly List<CPos> cells = new List<CPos>();
+		readonly LiquidTerrainLayer liquidTerrainLayer;
+		readonly List<CPos> floodedCells = new List<CPos>();
+		readonly ITemplatedTerrainInfo terrainInfo;
+		readonly Map map;
+		readonly DomainIndex domainIndex;
+		readonly CPos origin;
+		readonly Locomotor locomotor;
 
 		public Floods(Actor self, FloodsInfo info)
 			: base(info)
 		{
 			this.info = info;
+			liquidTerrainLayer = self.World.WorldActor.Trait<LiquidTerrainLayer>();
+			origin = self.Location;
+			floodedCells.Add(origin);
+			map = self.World.Map;
+			domainIndex = self.World.WorldActor.Trait<DomainIndex>();
+			locomotor = self.World.WorldActor.TraitsImplementing<Locomotor>().First(l => l.Info.Name == info.Locomotor);
 
-			resourceLayer = self.World.WorldActor.Trait<IResourceLayer>();
+			if (!(map.Rules.TerrainInfo is ITemplatedTerrainInfo terrainInfo))
+				throw new InvalidDataException($"{nameof(Floods)} requires a template-based tileset.");
 
-			cells.Add(self.Location);
+			this.terrainInfo = terrainInfo;
 		}
 
 		int ticks;
+		bool steadyWaveFront;
 
 		void ITick.Tick(Actor self)
 		{
 			if (IsTraitDisabled)
 				return;
 
+			if (steadyWaveFront)
+				return;
+
 			if (--ticks <= 0)
 			{
-				Flood(self);
+				Flood();
 				ticks = info.Interval;
 			}
 		}
 
-		public void Flood(Actor self)
+		int previousWaveFront;
+
+		public void Flood()
 		{
-			var waveFront = Util.ExpandFootprint(cells, true)
-				.Take(info.MaxRange)
-				.SkipWhile(p => !self.World.Map.Contains(p) ||
-					(resourceLayer.GetResource(p).Type == info.ResourceType))
+			var waveFront = Util.ExpandFootprint(floodedCells, true)
+				.Take(info.MaxTiles)
+				.SkipWhile(p => !map.Contains(p) || liquidTerrainLayer.ContainsTile(p))
 				.ToArray();
+
+			if (previousWaveFront == waveFront.Length)
+			{
+				steadyWaveFront = true;
+				return;
+			}
+
+			previousWaveFront = waveFront.Length;
 
 			foreach (var cell in waveFront)
 			{
-				if (resourceLayer.CanAddResource(info.ResourceType, cell))
-				{
-					cells.Add(cell);
-					resourceLayer.AddResource(info.ResourceType, cell, 1);
-				}
+				if (!info.StopTerrainTypes.Contains(map.GetTerrainInfo(cell).Type) && !domainIndex.IsPassable(origin, cell, locomotor))
+					continue;
+
+				var originalTile = map.Tiles[cell];
+				var replacement = info.ReplacementTiles[originalTile.Type];
+				var replacementTile = new TerrainTile(replacement, 0x00);
+				liquidTerrainLayer.AddTile(cell, replacementTile);
+				floodedCells.Add(cell);
 			}
 		}
 	}
---
+++ OpenRA.Mods.HV/Traits/World/LiquidTerrainLayer.cs
@@ -0,0 +1,142 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2021 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using OpenRA.Graphics;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.HV.Traits
+{
+	[Desc("Attach this to the world actor.")]
+	[TraitLocation(SystemActors.World)]
+	public class LiquidTerrainLayerInfo : TraitInfo, Requires<ITiledTerrainRendererInfo>
+	{
+		[Desc("Palette to render the layer sprites in.")]
+		public readonly string Palette = TileSet.TerrainPaletteInternalName;
+
+		public override object Create(ActorInitializer init) { return new LiquidTerrainLayer(init.Self, this); }
+	}
+
+	public class LiquidTerrainLayer : IRenderOverlay, IWorldLoaded, ITickRender, IRadarTerrainLayer, INotifyActorDisposing
+	{
+		public readonly CellLayer<bool> Covered;
+
+		readonly LiquidTerrainLayerInfo info;
+		readonly Dictionary<CPos, TerrainTile?> dirty = new Dictionary<CPos, TerrainTile?>();
+		readonly ITiledTerrainRenderer terrainRenderer;
+		readonly World world;
+		readonly CellLayer<(Color, Color)> radarColor;
+
+		TerrainSpriteLayer render;
+		PaletteReference paletteReference;
+		bool disposed;
+
+		public LiquidTerrainLayer(Actor self, LiquidTerrainLayerInfo info)
+		{
+			this.info = info;
+			world = self.World;
+			Covered = new CellLayer<bool>(world.Map);
+			radarColor = new CellLayer<(Color, Color)>(world.Map);
+			terrainRenderer = self.Trait<ITiledTerrainRenderer>();
+		}
+
+		void IWorldLoaded.WorldLoaded(World w, WorldRenderer wr)
+		{
+			render = new TerrainSpriteLayer(w, wr, terrainRenderer.MissingTile, BlendMode.Alpha, wr.World.Type != WorldType.Editor);
+			paletteReference = wr.Palette(info.Palette);
+		}
+
+		public bool ContainsTile(CPos cell)
+		{
+			return Covered.Contains(cell) && Covered[cell];
+		}
+
+		public void AddTile(CPos cell, TerrainTile tile)
+		{
+			if (!Covered.Contains(cell))
+				return;
+
+			var uv = cell.ToMPos(world.Map);
+			var tileInfo = world.Map.Rules.TerrainInfo.GetTerrainInfo(tile);
+			world.Map.CustomTerrain[uv] = tileInfo.TerrainType;
+			Covered[uv] = true;
+			radarColor[uv] = (tileInfo.GetColor(world.LocalRandom), tileInfo.GetColor(world.LocalRandom));
+			dirty[cell] = tile;
+		}
+
+		public void RemoveTile(CPos cell)
+		{
+			if (!Covered.Contains(cell))
+				return;
+
+			var uv = cell.ToMPos(world.Map);
+			world.Map.CustomTerrain[uv] = byte.MaxValue;
+			Covered[cell] = false;
+			radarColor[uv] = (Color.Transparent, Color.Transparent);
+			dirty[cell] = null;
+		}
+
+		void ITickRender.TickRender(WorldRenderer wr, Actor self)
+		{
+			var remove = new List<CPos>();
+			foreach (var kv in dirty)
+			{
+				if (!self.World.FogObscures(kv.Key))
+				{
+					var tile = kv.Value;
+					if (tile.HasValue)
+					{
+						// Terrain tiles define their origin at the topleft
+						var s = terrainRenderer.TileSprite(tile.Value);
+						var ss = new Sprite(s.Sheet, s.Bounds, s.ZRamp, float2.Zero, s.Channel, s.BlendMode);
+						render.Update(kv.Key, ss, paletteReference);
+					}
+					else
+						render.Clear(kv.Key);
+
+					remove.Add(kv.Key);
+				}
+			}
+
+			foreach (var r in remove)
+				dirty.Remove(r);
+		}
+
+		void IRenderOverlay.Render(WorldRenderer wr)
+		{
+			render.Draw(wr.Viewport);
+		}
+
+		event Action<CPos> IRadarTerrainLayer.CellEntryChanged
+		{
+			add => radarColor.CellEntryChanged += value;
+			remove => radarColor.CellEntryChanged -= value;
+		}
+
+		bool IRadarTerrainLayer.TryGetTerrainColorPair(MPos uv, out (Color Left, Color Right) value)
+		{
+			value = radarColor[uv];
+			return Covered[uv];
+		}
+
+		void INotifyActorDisposing.Disposing(Actor self)
+		{
+			if (disposed)
+				return;
+
+			render.Dispose();
+			disposed = true;
+		}
+	}
+}
