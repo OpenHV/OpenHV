f0a2f843e412946732ee59bb5b55c29a80c3a166
Matthias Mailänder <matthias@mailaender.name>
2021-12-30 17:40:55 +0100

    Improve forest performance significantly.

--- OpenRA.Mods.HV/Traits/World/ForestLayer.cs
+++ OpenRA.Mods.HV/Traits/World/ForestLayer.cs
@@ -1,6 +1,6 @@
 #region Copyright & License Information
 /*
- * Copyright 2019-2020 The OpenHV Developers (see CREDITS)
+ * Copyright 2019-2021 The OpenHV Developers (see CREDITS)
  * This file is part of OpenHV, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation, either version 3 of
@@ -9,72 +9,255 @@
  */
 #endregion
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using OpenRA.Graphics;
-using OpenRA.Mods.Common.Terrain;
+using OpenRA.Mods.Common.Effects;
+using OpenRA.Mods.Common.Traits;
 using OpenRA.Primitives;
 using OpenRA.Traits;
 
 namespace OpenRA.Mods.HV.Traits
 {
-	class ForestLayerInfo : TraitInfo
+	[Desc("Attach this to the world actor.")]
+	[TraitLocation(SystemActors.World)]
+	class ForestLayerInfo : TraitInfo, IOccupySpaceInfo, IRulesetLoaded
 	{
-		[ActorReference]
-		[FieldLoader.Require]
-		public readonly string[] Trees = null;
+		[Desc("Palette to render the layer sprites in.")]
+		public readonly string Palette = TileSet.TerrainPaletteInternalName;
 
-		public override object Create(ActorInitializer init) { return new ForestLayer(this); }
+		[Desc("Who should be allowed to crush a tree.")]
+		public readonly BitSet<CrushClass> CrushClasses = new BitSet<CrushClass>("Tree");
+
+		[Desc("Initial max amount per tile.")]
+		public readonly int Hitpoints = 100000;
+
+		[Desc("At which health level to display flames.")]
+		public readonly int DamagedHitpoints = 75000;
+
+		[FieldLoader.Require]
+		[Desc("Animation image.")]
+		public readonly string Image = null;
+
+		[SequenceReference(nameof(Image))]
+		[Desc("Animation to play when damaged.")]
+		public readonly string Sequence = null;
+
+		[Desc("Time in ticks to spawn a new flame and to apply damage.")]
+		public readonly int Interval = 8;
+
+		[Desc("How much damage to apply to neighboring tiles when on fire.")]
+		public readonly int Damage = 10;
+
+		[FieldLoader.Require]
+		[Desc("Terrain types a tree can change into.")]
+		public readonly string[] TransformedTerrain = Array.Empty<string>();
+
+		[FieldLoader.Require]
+		[Desc("Which tile ID to replace with which munched variant")]
+		public readonly Dictionary<ushort, ushort> CrushedTiles = new Dictionary<ushort, ushort>();
+
+		[FieldLoader.Require]
+		[Desc("Which tile ID to replace with which scorched variant")]
+		public readonly Dictionary<ushort, ushort> BurnedTiles = new Dictionary<ushort, ushort>();
+
+		public void RulesetLoaded(Ruleset rules, ActorInfo actorInfo)
+		{
+			var distinctCrushedTiles = CrushedTiles.Keys.Distinct().Count();
+			if (CrushedTiles.Keys.Count > distinctCrushedTiles)
+				throw new YamlException($"Duplicate tile in {nameof(CrushedTiles)}.");
+
+			var distinctBurnedTiles = BurnedTiles.Keys.Distinct().Count();
+			if (BurnedTiles.Keys.Count > distinctBurnedTiles)
+				throw new YamlException($"Duplicate tile in {nameof(BurnedTiles)}.");
+
+			if (distinctCrushedTiles != distinctBurnedTiles)
+				throw new YamlException($"{nameof(CrushedTiles)} does not match {nameof(BurnedTiles)}.");
+		}
+
+		public IReadOnlyDictionary<CPos, SubCell> OccupiedCells(ActorInfo info, CPos location, SubCell subCell = SubCell.Any)
+		{
+			return new Dictionary<CPos, SubCell>() { { location, SubCell.FullCell } };
+		}
+
+		bool IOccupySpaceInfo.SharesCell => false;
+
+		public override object Create(ActorInitializer init) { return new ForestLayer(init.Self, this); }
 	}
 
-	class ForestLayer : IWorldLoaded
+	class ForestLayer : IWorldLoaded, IOccupySpace, ICrushable, INotifyCrushed, ITick, ITickRender, IRenderOverlay, IRadarTerrainLayer
 	{
 		readonly ForestLayerInfo info;
-		readonly Dictionary<ushort, string> treeTypes = new Dictionary<ushort, string>();
+		readonly World world;
+		readonly CellLayer<int> hitpoints;
+		readonly ITiledTerrainRenderer terrainRenderer;
+		readonly CellLayer<(Color, Color)> radarColor;
+		readonly Dictionary<CPos, TerrainTile?> dirty;
+		readonly List<(CPos cell, SubCell subCell)> occupied;
 
-		CellLayer<Tree> trees;
+		TerrainSpriteLayer render;
+		PaletteReference paletteReference;
 
-		public ForestLayer(ForestLayerInfo info)
+		int ticks;
+
+		public ForestLayer(Actor self, ForestLayerInfo info)
 		{
 			this.info = info;
+			world = self.World;
+			hitpoints = new CellLayer<int>(world.Map);
+			terrainRenderer = self.Trait<ITiledTerrainRenderer>();
+			radarColor = new CellLayer<(Color, Color)>(world.Map);
+			world.UpdateMaps(self, this);
+			occupied = new List<(CPos, SubCell)>();
+			dirty = new Dictionary<CPos, TerrainTile?>();
+			ticks = info.Interval;
 		}
 
 		public void WorldLoaded(World w, WorldRenderer wr)
 		{
-			trees = new CellLayer<Tree>(w.Map);
+			render = new TerrainSpriteLayer(w, wr, terrainRenderer.MissingTile, BlendMode.Alpha, wr.World.Type != WorldType.Editor);
+			paletteReference = wr.Palette(info.Palette);
 
-			// Build a list of templates that should be overlayed with trees
-			foreach (var tree in info.Trees)
+			var treeTiles = info.CrushedTiles.Keys;
+			foreach (var forestCell in w.Map.AllCells.Where(cell => treeTiles.Contains(w.Map.Tiles[cell].Type)))
 			{
-				var treeInfo = w.Map.Rules.Actors[tree].TraitInfo<TreeInfo>();
-				treeTypes.Add(treeInfo.Template, tree);
+				hitpoints[forestCell] = info.Hitpoints;
+				occupied.Add((forestCell, SubCell.FullCell));
 			}
 
-			// Take all templates to overlay from the map
-			foreach (var cell in w.Map.AllCells.Where(cell => treeTypes.ContainsKey(w.Map.Tiles[cell].Type)))
-				ConvertTreeToActor(w, cell);
+			w.ActorMap.AddInfluence(w.WorldActor, this);
 		}
 
-		void ConvertTreeToActor(World w, CPos cell)
+		public CPos TopLeft => CPos.Zero;
+		public WPos CenterPosition => WPos.Zero;
+		public (CPos, SubCell)[] OccupiedCells() { return occupied.ToArray(); }
+
+		bool ICrushable.CrushableBy(Actor self, Actor crusher, BitSet<CrushClass> crushClasses)
 		{
-			// This cell already has a tree overlaying it from a previous iteration
-			if (trees[cell] != null)
+			return info.CrushClasses.Overlaps(crushClasses);
+		}
+
+		LongBitSet<PlayerBitMask> ICrushable.CrushableBy(Actor self, BitSet<CrushClass> crushClasses)
+		{
+			if (!info.CrushClasses.Overlaps(crushClasses))
+				return self.World.NoPlayersMask;
+
+			return self.World.AllPlayersMask;
+		}
+
+		void INotifyCrushed.WarnCrush(Actor self, Actor crusher, BitSet<CrushClass> crushClasses) { }
+		void INotifyCrushed.OnCrush(Actor self, Actor crusher, BitSet<CrushClass> crushClasses)
+		{
+			var cell = crusher.Location;
+			var originalTile = self.World.Map.Tiles[cell];
+			var replacement = info.CrushedTiles[originalTile.Type];
+			var replacementTile = new TerrainTile(replacement, 0x00);
+			ReplaceTile(cell, replacementTile);
+		}
+
+		void ReplaceTile(CPos cell, TerrainTile replacementTile)
+		{
+			dirty[cell] = replacementTile;
+
+			var uv = cell.ToMPos(world.Map);
+			var tileInfo = world.Map.Rules.TerrainInfo.GetTerrainInfo(replacementTile);
+			world.Map.CustomTerrain[uv] = tileInfo.TerrainType;
+			hitpoints[uv] = 0;
+			radarColor[uv] = (tileInfo.GetColor(world.LocalRandom), tileInfo.GetColor(world.LocalRandom));
+
+			occupied.RemoveAll(o => o.cell == cell);
+			world.ActorMap.UpdateOccupiedCells(this);
+		}
+
+		public void Hit(CPos cell, int damage)
+		{
+			if (!hitpoints.Contains(cell) || hitpoints[cell] <= 0)
 				return;
 
-			// Correlate the tile nameof(Image) aka subtile with its position to find the template origin
-			var tile = w.Map.Tiles[cell].Type;
-			var index = w.Map.Tiles[cell].Index;
-			var terrainInfo = w.Map.Rules.TerrainInfo as ITemplatedTerrainInfo;
-			var template = terrainInfo.Templates[tile];
-			var ni = cell.X - index % template.Size.X;
-			var nj = cell.Y - index / template.Size.X;
+			hitpoints[cell] = hitpoints[cell] - damage;
 
-			// Create a new actor for this tree and keep track of which subtiles this tree includes
-			var tree = w.CreateActor(treeTypes[tile], new TypeDictionary
+			if (hitpoints[cell] < 1)
 			{
-				new LocationInit(new CPos(ni, nj)),
-				new OwnerInit(w.WorldActor.Owner),
-			});
+				var originalTile = world.Map.Tiles[cell];
+				var replacement = info.BurnedTiles[originalTile.Type];
+				var replacementTile = new TerrainTile(replacement, 0x00);
+				ReplaceTile(cell, replacementTile);
+			}
+		}
+
+		void ITick.Tick(Actor self)
+		{
+			if (--ticks > 0)
+				return;
+
+			foreach (var cell in world.Map.AllCells)
+			{
+				var hp = hitpoints[cell];
+				if (hp > 0 && hp < info.DamagedHitpoints)
+				{
+					var position = world.Map.CenterOfCell(cell);
+					world.AddFrameEndTask(w => w.Add(new SpriteEffect(position, w, info.Image, info.Sequence, info.Palette)));
+
+					Hit(cell, info.Damage);
+					foreach (var direction in CVec.Directions)
+						Hit(cell + direction, info.Damage);
+				}
+			}
+
+			ticks = info.Interval;
+		}
+
+		void ITickRender.TickRender(WorldRenderer wr, Actor self)
+		{
+			var remove = new List<CPos>();
+			foreach (var kv in dirty)
+			{
+				if (!self.World.FogObscures(kv.Key))
+				{
+					var tile = kv.Value;
+					if (tile.HasValue)
+					{
+						// Terrain tiles define their origin at the topleft
+						var s = terrainRenderer.TileSprite(tile.Value);
+						var sprite = new Sprite(s.Sheet, s.Bounds, s.ZRamp, float2.Zero, s.Channel, s.BlendMode);
+						render.Update(kv.Key, sprite, paletteReference);
+					}
+					else
+						render.Clear(kv.Key);
+
+					remove.Add(kv.Key);
+				}
+			}
+
+			foreach (var r in remove)
+				dirty.Remove(r);
+		}
+
+		void IRenderOverlay.Render(WorldRenderer wr)
+		{
+			render.Draw(wr.Viewport);
+		}
+
+		event Action<CPos> IRadarTerrainLayer.CellEntryChanged
+		{
+			add => radarColor.CellEntryChanged += value;
+			remove => radarColor.CellEntryChanged -= value;
+		}
+
+		bool IRadarTerrainLayer.TryGetTerrainColorPair(MPos uv, out (Color Left, Color Right) value)
+		{
+			value = default;
+
+			if (world.Map.CustomTerrain[uv] == byte.MaxValue)
+				return false;
+
+			var cell = uv.ToCPos(world.Map);
+			if (!info.TransformedTerrain.Contains(world.Map.GetTerrainInfo(cell).Type))
+				return false;
+
+			value = radarColor[uv];
+			return true;
 		}
 	}
 }
---
+++ OpenRA.Mods.HV/Warheads/TreeDamageWarhead.cs
@@ -0,0 +1,87 @@
+#region Copyright & License Information
+/*
+ * Copyright 2021 The OpenHV Developers (see CREDITS)
+ * This file is part of OpenHV, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Linq;
+using OpenRA.GameRules;
+using OpenRA.Mods.Common;
+using OpenRA.Mods.Common.Warheads;
+using OpenRA.Mods.HV.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.HV.Warheads
+{
+	[Desc("A simplified " + nameof(SpreadDamageWarhead) + " that interacts with " + nameof(ForestLayer))]
+	public class TreeDamageWarhead : DamageWarhead, IRulesetLoaded<WeaponInfo>
+	{
+		[Desc("Range between falloff steps.")]
+		public readonly WDist Spread = new WDist(43);
+
+		[Desc("Damage percentage at each range step")]
+		public readonly int[] Falloff = { 100, 37, 14, 5, 0 };
+
+		[Desc("Ranges at which each Falloff step is defined. Overrides Spread.")]
+		public WDist[] Range = null;
+
+		[Desc("How much damage to apply to wood armor.")]
+		public int Percentage = 100;
+
+		void IRulesetLoaded<WeaponInfo>.RulesetLoaded(Ruleset rules, WeaponInfo info)
+		{
+			if (Range != null)
+			{
+				if (Range.Length != 1 && Range.Length != Falloff.Length)
+					throw new YamlException("Number of range values must be 1 or equal to the number of Falloff values.");
+
+				for (var i = 0; i < Range.Length - 1; i++)
+					if (Range[i] > Range[i + 1])
+						throw new YamlException("Range values must be specified in an increasing order.");
+			}
+			else
+				Range = Exts.MakeArray(Falloff.Length, i => i * Spread);
+		}
+
+		protected override void DoImpact(WPos pos, Actor firedBy, WarheadArgs args)
+		{
+			var layer = firedBy.World.WorldActor.Trait<ForestLayer>();
+			var minRange = WDist.Zero.Length / 1024;
+			var maxRange = Range[Range.Length - 1].Length / 1024;
+			var impactCell = firedBy.World.Map.CellContaining(pos);
+			foreach (var cell in firedBy.World.Map.FindTilesInAnnulus(impactCell, minRange, maxRange))
+			{
+				var falloffDistance = (firedBy.World.Map.CenterOfCell(cell) - pos).Length;
+
+				// The range to target is more than the range the warhead covers,
+				// so GetDamageFalloff() is going to give us 0 and we're going to do 0 damage anyway, so bail early.
+				if (falloffDistance > Range[Range.Length - 1].Length)
+					continue;
+
+				var localModifiers = args.DamageModifiers.Append(GetDamageFalloff(falloffDistance)).ToArray();
+				var damage = Util.ApplyPercentageModifiers(Damage, localModifiers.Append(Percentage));
+				layer.Hit(cell, damage);
+			}
+		}
+
+		int GetDamageFalloff(int distance)
+		{
+			var inner = Range[0].Length;
+			for (var i = 1; i < Range.Length; i++)
+			{
+				var outer = Range[i].Length;
+				if (outer > distance)
+					return int2.Lerp(Falloff[i - 1], Falloff[i], distance - inner, outer - inner);
+
+				inner = outer;
+			}
+
+			return 0;
+		}
+	}
+}
