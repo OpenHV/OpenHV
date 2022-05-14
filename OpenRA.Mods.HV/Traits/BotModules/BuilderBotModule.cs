3d692184f9dbb30434f240afab85dada806f6f28
Matthias Mailänder <matthias@mailaender.name>
2022-05-14 22:32:52 +0200

    Fix Using directives should be ordered alphabetically by the namespaces.

--- OpenRA.Mods.HV/Traits/BotModules/BuilderBotModule.cs
+++ OpenRA.Mods.HV/Traits/BotModules/BuilderBotModule.cs
@@ -12,9 +12,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using OpenRA.Traits;
 using OpenRA.Mods.Common;
 using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
 
 namespace OpenRA.Mods.HV.Traits
 {
