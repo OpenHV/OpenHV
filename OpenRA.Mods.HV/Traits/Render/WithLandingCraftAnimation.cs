fa23b4d2886e0edb25827cf58b66e62c27592fd6
Matthias Mailänder <matthias@mailaender.name>
2022-12-17 19:15:32 +0100

    Clear is actually just a debug terrain type.

--- OpenRA.Mods.HV/Traits/Render/WithLandingCraftAnimation.cs
+++ OpenRA.Mods.HV/Traits/Render/WithLandingCraftAnimation.cs
@@ -1,6 +1,6 @@
 #region Copyright & License Information
 /*
- * Copyright 2007-2022 The OpenHV Developers (see AUTHORS)
+ * Copyright 2022 The OpenHV Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation, either version 3 of
@@ -19,7 +19,8 @@ namespace OpenRA.Mods.HV.Traits.Render
 {
 	public class WithLandingCraftAnimationInfo : TraitInfo, Requires<IMoveInfo>, Requires<WithSpriteBodyInfo>, Requires<CargoInfo>
 	{
-		public readonly HashSet<string> OpenTerrainTypes = new HashSet<string> { "Clear" };
+		[FieldLoader.Require]
+		public readonly HashSet<string> OpenTerrainTypes = null;
 
 		[SequenceReference]
 		public readonly string OpenSequence = "open";
