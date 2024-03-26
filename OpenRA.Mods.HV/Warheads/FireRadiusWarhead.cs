c0092c57445c01b863aadb09abcd39b02208856b
Matthias Mailänder <matthias@mailaender.name>
2024-03-26 12:47:37 +0100

    Remove unused import.

--- OpenRA.Mods.HV/Warheads/FireRadiusWarhead.cs
+++ OpenRA.Mods.HV/Warheads/FireRadiusWarhead.cs
@@ -12,7 +12,6 @@
 using System;
 using System.Linq;
 using OpenRA.GameRules;
-using OpenRA.Mods.Common.Activities;
 using OpenRA.Mods.Common.Traits;
 using OpenRA.Mods.Common.Warheads;
 using OpenRA.Traits;
