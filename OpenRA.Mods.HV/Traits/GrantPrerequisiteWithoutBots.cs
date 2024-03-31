7e6f60be49fe03693f4c7a1c33e2cf35b301d198
Matthias Mailänder <matthias@mailaender.name>
2024-03-31 22:40:31 +0200

    Only disable barriers in bot games.

---
+++ OpenRA.Mods.HV/Traits/GrantPrerequisiteWithoutBots.cs
@@ -0,0 +1,52 @@
+#region Copyright & License Information
+/*
+ * Copyright 2024 The OpenHV Developers (see CREDITS)
+ * This file is part of OpenHV, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using System.Linq;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.HV.Traits
+{
+	[Desc("Grants a prerequisite for human only games.")]
+	public class GrantPrerequisiteWithoutBotsInfo : TraitInfo, ITechTreePrerequisiteInfo
+	{
+		[FieldLoader.Require]
+		[Desc("The prerequisite type that this provides.")]
+		public readonly string Prerequisite = null;
+
+		IEnumerable<string> ITechTreePrerequisiteInfo.Prerequisites(ActorInfo info)
+		{
+			yield return Prerequisite;
+		}
+
+		public override object Create(ActorInitializer init) { return new GrantPrerequisiteWithoutBots(init.Self, this); }
+	}
+
+	public class GrantPrerequisiteWithoutBots : ITechTreePrerequisite, INotifyCreated
+	{
+		readonly string[] prerequisites;
+
+		bool botGame;
+
+		public GrantPrerequisiteWithoutBots(Actor self, GrantPrerequisiteWithoutBotsInfo info)
+		{
+			prerequisites = new[] { info.Prerequisite };
+		}
+
+		void INotifyCreated.Created(Actor self)
+		{
+			botGame = self.World.Players.Any(p => p.IsBot);
+		}
+
+		IEnumerable<string> ITechTreePrerequisite.ProvidesPrerequisites => botGame ? Enumerable.Empty<string>() : prerequisites;
+	}
+}
