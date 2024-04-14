53ee7364731addd2a55f01a1ce89cc86ed4f8d4d
Matthias Mailänder <matthias@mailaender.name>
2024-04-14 11:33:25 +0200

    Disallow drop pod deployment on unlandable terrain.
    Closes #944

--- OpenRA.Mods.HV/Traits/SupportPowers/DropPodsPower.cs
+++ OpenRA.Mods.HV/Traits/SupportPowers/DropPodsPower.cs
@@ -1,6 +1,6 @@
 #region Copyright & License Information
 /*
- * Copyright 2021 The OpenHV Developers (see CREDITS)
+ * Copyright 2021-2024 The OpenHV Developers (see CREDITS)
  * This file is part of OpenHV, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation, either version 3 of
@@ -9,9 +9,12 @@
  */
 #endregion
 
+using System.Collections.Generic;
 using System.Linq;
+using OpenRA.Graphics;
 using OpenRA.Mods.Common;
 using OpenRA.Mods.Common.Activities;
+using OpenRA.Mods.Common.Orders;
 using OpenRA.Mods.Common.Traits;
 using OpenRA.Primitives;
 using OpenRA.Traits;
@@ -41,17 +44,43 @@ namespace OpenRA.Mods.HV.Traits
 		[Desc("Number of ticks to keep the camera alive")]
 		public readonly int CameraRemoveDelay = 25;
 
+		[CursorReference]
+		[Desc("Cursor when unable to activate on this position. ")]
+		public readonly string BlockedCursor = "generic-blocked";
+
 		public override object Create(ActorInitializer init) { return new DropPodsPower(init.Self, this); }
 	}
 
 	public class DropPodsPower : SupportPower
 	{
 		readonly DropPodsPowerInfo info;
+		readonly AircraftInfo aircraftInfo;
+		readonly FallsToEarthInfo fallsToEarthInfo;
 
 		public DropPodsPower(Actor self, DropPodsPowerInfo info)
 			: base(self, info)
 		{
 			this.info = info;
+
+			var actorInfo = self.World.Map.Rules.Actors[info.UnitTypes[0].ToLowerInvariant()];
+			aircraftInfo = actorInfo.TraitInfo<AircraftInfo>();
+			fallsToEarthInfo = actorInfo.TraitInfo<FallsToEarthInfo>();
+		}
+
+		public override void SelectTarget(Actor self, string order, SupportPowerManager manager)
+		{
+			self.World.OrderGenerator = new SelectDropPodsTarget(order, manager, this, MouseButton.Left);
+		}
+
+		public bool Validate(World world, CPos cell)
+		{
+			if (!world.Map.Contains(cell))
+				return false;
+
+			if (!aircraftInfo.LandableTerrainTypes.Contains(world.Map.GetTerrainInfo(cell).Type))
+				return false;
+
+			return true;
 		}
 
 		public override void Activate(Actor self, Order order, SupportPowerManager manager)
@@ -63,11 +92,9 @@ namespace OpenRA.Mods.HV.Traits
 
 		public void SendDropPods(Actor self, Order order, WAngle facing)
 		{
-			var actorInfo = self.World.Map.Rules.Actors[info.UnitTypes[0].ToLowerInvariant()];
-			var aircraftInfo = actorInfo.TraitInfo<AircraftInfo>();
 			var altitude = aircraftInfo.CruiseAltitude.Length;
 			var approachRotation = WRot.FromYaw(facing);
-			var fallsToEarthInfo = actorInfo.TraitInfo<FallsToEarthInfo>();
+
 			var delta = new WVec(0, -altitude * aircraftInfo.Speed / fallsToEarthInfo.Velocity.Length, 0).Rotate(approachRotation);
 
 			self.World.AddFrameEndTask(w =>
@@ -117,6 +144,52 @@ namespace OpenRA.Mods.HV.Traits
 						w.Add(pod);
 				}
 			});
+		}
+	}
+
+	public class SelectDropPodsTarget : OrderGenerator
+	{
+		readonly SupportPowerManager manager;
+		readonly DropPodsPower power;
+		readonly MouseButton expectedButton;
+
+		public string OrderKey { get; }
+
+		public SelectDropPodsTarget(string order, SupportPowerManager manager, DropPodsPower power, MouseButton button)
+		{
+			if (Game.Settings.Game.UseClassicMouseStyle)
+				manager.Self.World.Selection.Clear();
+
+			this.manager = manager;
+			this.power = power;
+			OrderKey = order;
+			expectedButton = button;
+		}
+
+		protected override IEnumerable<Order> OrderInner(World world, CPos cell, int2 worldPixel, MouseInput mi)
+		{
+			world.CancelInputMode();
+
+			if (!power.Validate(world, cell))
+				yield break;
+
+			if (mi.Button == expectedButton)
+				yield return new Order(OrderKey, manager.Self, Target.FromCell(world, cell), false) { SuppressVisualFeedback = true };
+		}
+
+		protected override void Tick(World world)
+		{
+			if (!manager.Powers.TryGetValue(OrderKey, out var p) || !p.Active || !p.Ready)
+				world.CancelInputMode();
+		}
+
+		protected override IEnumerable<IRenderable> Render(WorldRenderer wr, World world) { yield break; }
+		protected override IEnumerable<IRenderable> RenderAboveShroud(WorldRenderer wr, World world) { yield break; }
+		protected override IEnumerable<IRenderable> RenderAnnotations(WorldRenderer wr, World world) { yield break; }
+		protected override string GetCursor(World world, CPos cell, int2 worldPixel, MouseInput mi)
+		{
+			var info = (DropPodsPowerInfo)power.Info;
+			return power.Validate(world, cell) ? power.Info.Cursor : info.BlockedCursor;
 		}
 	}
 }
