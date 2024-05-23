176ca2cf3e6cf56cca66d2a06f67885438f8c7ee
Cromha <87318892+OcelotWalrus@users.noreply.github.com>
2024-05-23 17:54:48 +0200

    Update mods/hv/maps/asteroid-invasion/asteroid-invasion.lua
    Co-authored-by: Matthias Mailänder <Mailaender@users.noreply.github.com>

--- mods/hv/maps/asteroid-invasion/asteroid-invasion.lua
+++ mods/hv/maps/asteroid-invasion/asteroid-invasion.lua
@@ -1,5 +1,5 @@
 --[[
-   Copyright 2021-2023 The OpenHV Developers (see AUTHORS)
+   Copyright 2024 The OpenHV Developers (see AUTHORS)
    This file is part of OpenHV, which is free software. It is made
    available to you under the terms of the GNU General Public License
    as published by the Free Software Foundation, either version 3 of
