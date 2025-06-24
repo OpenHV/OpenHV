a030be127ce398a666914b514b5245f8652ae845
Matthias Mailänder <matthias@mailaender.name>
2025-06-24 15:01:25 +0200

    Don't render aircraft shadows in the encylopedia.
    Closes #1292.

--- OpenRA.Mods.HV/Traits/Render/WithIdleOverlayOnGround.cs
+++ OpenRA.Mods.HV/Traits/Render/WithIdleOverlayOnGround.cs
@@ -9,52 +9,36 @@
  */
 #endregion
 
-using System;
-using System.Collections.Generic;
 using OpenRA.Graphics;
-using OpenRA.Mods.Common;
-using OpenRA.Mods.Common.Graphics;
 using OpenRA.Mods.Common.Traits;
 using OpenRA.Mods.Common.Traits.Render;
 using OpenRA.Traits;
 
 namespace OpenRA.Mods.HV.Traits.Render
 {
-	[Desc("Plays an idle overlay on the ground position under the actor (regardless of it's actual height).")]
-	public class WithIdleOverlayOnGroundInfo : WithIdleOverlayInfo
+	[Desc("Renders a sprite on the ground position under the actor (regardless of it's actual height).")]
+	public class WithIdleOverlayOnGroundInfo : PausableConditionalTraitInfo, Requires<RenderSpritesInfo>, Requires<BodyOrientationInfo>
 	{
+		[Desc("Image used for this decoration. Defaults to the actor's type.")]
+		public readonly string Image = null;
+
+		[SequenceReference(nameof(Image), allowNullImage: true)]
+		[Desc("Sequence name to use")]
+		public readonly string Sequence = "idle-overlay";
+
+		[Desc("Position relative to body")]
+		public readonly WVec Offset = WVec.Zero;
+
+		[PaletteReference(nameof(IsPlayerPalette))]
+		[Desc("Custom palette name")]
+		public readonly string Palette = null;
+
+		[Desc("Custom palette is a player palette BaseName")]
+		public readonly bool IsPlayerPalette = false;
+
+		public readonly bool IsDecoration = false;
+
 		public override object Create(ActorInitializer init) { return new WithIdleOverlayOnGround(init.Self, this); }
-
-		public new IEnumerable<IActorPreview> RenderPreviewSprites(ActorPreviewInitializer init, string image, int facings, PaletteReference p)
-		{
-			if (!EnabledByDefault)
-				yield break;
-
-			if (Palette != null)
-				p = init.WorldRenderer.Palette(Palette);
-
-			Func<WAngle> facing;
-			var dynamicfacingInit = init.GetOrDefault<DynamicFacingInit>(this);
-			if (dynamicfacingInit != null)
-				facing = dynamicfacingInit.Value;
-			else
-			{
-				var f = init.GetValue<FacingInit, WAngle>(this, WAngle.Zero);
-				facing = () => f;
-			}
-
-			var anim = new Animation(init.World, image, facing)
-			{
-				IsDecoration = IsDecoration
-			};
-
-			anim.PlayRepeating(RenderSprites.NormalizeSequence(anim, init.GetDamageState(), Sequence));
-
-			var body = init.Actor.TraitInfo<BodyOrientationInfo>();
-			var orientation = body.QuantizeOrientation(WRot.FromYaw(facing()), facings);
-			var offset = body.LocalToWorld(Offset.Rotate(orientation));
-			yield return new SpriteActorPreview(anim, () => offset, () => offset.Y + offset.Z + 1, p);
-		}
 	}
 
 	public class WithIdleOverlayOnGround : PausableConditionalTrait<WithIdleOverlayOnGroundInfo>, INotifyDamageStateChanged
@@ -75,11 +59,7 @@ namespace OpenRA.Mods.HV.Traits.Render
 				IsDecoration = info.IsDecoration
 			};
 
-			if (info.StartSequence != null)
-				overlay.PlayThen(RenderSprites.NormalizeSequence(overlay, self.GetDamageState(), info.StartSequence),
-					() => overlay.PlayRepeating(RenderSprites.NormalizeSequence(overlay, self.GetDamageState(), info.Sequence)));
-			else
-				overlay.PlayRepeating(RenderSprites.NormalizeSequence(overlay, self.GetDamageState(), info.Sequence));
+			overlay.PlayRepeating(RenderSprites.NormalizeSequence(overlay, self.GetDamageState(), info.Sequence));
 
 			var anim = new AnimationWithOffset(overlay,
 				() => body.LocalToWorld(info.Offset.Rotate(body.QuantizeOrientation(self.Orientation)))
